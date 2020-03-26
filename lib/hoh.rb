ad85601bcc7020fae26a2d6fceafe29b4ba6e23c
Mack McQuen <mackmcquen@comcast.net>
2020-03-26 20:48:06 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,16 +44,22 @@ def naming_system
   hash = {
      :label => "Kingdom",
     :sub_category => {
-     :label => "Phylum" },
+     :label => "Phylum",
     :sub_category => {
-     :label => "Class" },
+     :label => "Class",
     :sub_category => {
-      :label => "Order" },
+      :label => "Order",
     :sub_category => {
-      :label => "Family" },
+      :label => "Family",
     :sub_category => {
-      :label => "Genus" },
+      :label => "Genus",
     :sub_category => {
-      :label => "Species" }
+      :label => "Species"
+    }
+    }
+    }
+    }
+    }
+    }
   }
 end
